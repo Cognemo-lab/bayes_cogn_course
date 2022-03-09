46ea0dfc21d697722f5201840c7f8add5e9de527
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2022-03-09 15:42:35 -0500

    Removed seed shuffling from simulation models.

--- Toolboxes/tapas/tapas_condhalluc_obs3_sim.m
+++ Toolboxes/tapas/tapas_condhalluc_obs3_sim.m
@@ -27,13 +27,6 @@ x = mu1hat + 1/(1 + nu)*(tp - mu1hat);
 % Apply the logistic sigmoid to the inferred beliefs
 prob = tapas_sgm(be.*(2.*x-1),1);
 
-% Initialize random number generator
-if isnan(r.c_sim.seed)
-    rng('shuffle');
-else
-    rng(r.c_sim.seed);
-end
-
 % Simulate
 y = binornd(1, prob);
 
